9b358baf33cf4060b2e26d081792bc51205cd6a3
Ulrich Diedrichsen <ulrich.diedrichsen@googlemail.com>
2015-04-30 22:23:52 +0200

    Packages version added

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use("mjn:famous");
+  api.use("mjn:famous@0.3.5",'client');
   api.addFiles('famous-angular.css','client');
   api.addFiles('famous-angular.js','client');
 });
