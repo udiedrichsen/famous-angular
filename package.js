2fa9473ca988210b21c62898385ef499905d2bd2
Ulrich Diedrichsen <ulrich.diedrichsen@googlemail.com>
2015-05-01 12:27:43 +0200

    Use angularjs instead of uri angular

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'doedel:famous-angular',
-  version: '0.5.2_1',
+  version: '0.5.2_2',
   // Brief, one-line summary of the package.
   summary: 'Bring structure to your Famo.us apps with the power of AngularJS',
   // URL to the Git repository containing the source code for this package.
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use('mjn:famous@0.3.5','client');
-  api.use('urigo:angular@0.8.7','client');
+  api.use('angular:angular@1.3.15_1', 'client');
   api.addFiles('lib/famous-angular.min.css','client');
   api.addFiles('lib/famous-angular.min.js','client');
 });
